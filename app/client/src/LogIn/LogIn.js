75d76b3ccb84069143d69ca70c4f9886aeeac6b4
Kevin Tenolli <tenollikevin@gmail.com>
2023-04-02 17:19:10 +0200

    initial testsfor react app

---
+++ app/client/src/LogIn/LogIn.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can add your login logic
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </label>
+      <br />
+      <Button variant="contained" type="submit">
+        Login
+      </Button>
+    </form>
+  );
+};
+
+export default Login;
