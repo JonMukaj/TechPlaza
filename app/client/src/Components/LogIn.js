4805d1081329afbb24c482988b663dff07077240
Kevin Tenolli <105079235+ProfessorGustavi@users.noreply.github.com>
2023-04-08 14:05:00 +0200

    feat: initial test setup (#4)

---
+++ app/client/src/Components/LogIn.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import axios from "axios";
+
+const Root = styled("form")(({ theme }) => ({
+  "& .MuiTextField-root": {
+    margin: theme.spacing(1),
+    width: "25ch",
+  },
+}));
+
+const SubmitButton = styled(Button)(({ theme }) => ({
+  margin: theme.spacing(1),
+}));
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = { email, password };
+    console.log(data);
+    axios.post("http://localhost:5000/login", data).then((res) => {
+      console.log(res);
+    });
+  };
+
+  return (
+    <Root onSubmit={handleSubmit}>
+      <TextField
+        label="Email"
+        type="email"
+        variant="outlined"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <TextField
+        label="Password"
+        variant="outlined"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <SubmitButton variant="contained" color="primary" type="submit">
+        Login
+      </SubmitButton>
+    </Root>
+  );
+};
+
+export default Login;
