8bb10cb9e510391d5eb25dd024699972547b4aae
fabio <fabiomarku2001@gmail.com>
2023-04-16 11:11:43 +0200

    ch

--- app/server/server.js
+++ app/server/server.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const  errorHandler  = require('./errorHandler');
+//const userRoutes = require('./routes/userRoutes');
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require("cors");
 const path = require("path");
+
 
 const whitelist = ["http://localhost:3000"];
 app.use(express.json());
 app.use(cors(whitelist));
+// Middleware
+//app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -20,8 +28,12 @@ app.get('/', (req, res) => {
 });
 
 app.get("/backend", (req, res) => {
-  res.send({ express: "TOP G!" });
+  res.send({ express: "TOP G!iiisa" });
 });
+
+
+//app.use('/users', userRoutes);
+//app.use(errorHandler);
 
 //  Create a login route
 app.post("/login", (req, res) => {
@@ -29,3 +41,6 @@ app.post("/login", (req, res) => {
   const { email, password } = req.body;
   res.send({ email, password });
 });
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`));
