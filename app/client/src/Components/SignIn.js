6d4c9e9dca90162b68692a6833efdc8ede7e12cb
JonMukaj <j.mukaj.ext@teamsystem.com>
2023-04-17 19:08:05 +0200

    add pipeline check for other branches

--- app/client/src/Components/SignIn.js
+++ app/client/src/Components/SignIn.js
@@ -13,7 +13,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Link } from "react-router-dom";
-import axios from "./api/axios";
+import axios from "../api/axios";
 
 const LOGIN_URL = "/login";
 
