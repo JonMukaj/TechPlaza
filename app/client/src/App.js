2bf658a8e63ed0ff6594c3741247ea93d6da0345
Kevin Tenolli <tenollikevin@gmail.com>
2023-04-18 22:55:41 +0200

    feat: Add product list page and filters

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -5,6 +5,7 @@ import SignIn from "./Components/SignIn";
 import SignUp from "./Components/SignUp";
 import MainPage from "./Components/MainPage";
 import Navbar from "./Components/Navbar";
+import ProductList from "./Components/ProductList";
 
 function App() {
   let isAuthenticated = false;
@@ -16,6 +17,8 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<MainPage />} />
+        <Route path="/category/:category" element={<ProductList />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
   );
